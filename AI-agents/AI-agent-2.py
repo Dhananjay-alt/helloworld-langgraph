3acac687e0185dacad613358c50df90d720ed965
Dhananjay Hire <coderxdhananjay07@gmail.com>
2026-04-16 16:35:49 +0530

    Made a AI chatbot with memory

---
+++ AI-agents/AI-agent-2.py
@@ -0,0 +1,45 @@
+# Chatbot 
+
+import os
+from typing import TypedDict, List, Union
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_openai import ChatOpenAI
+from langgraph.graph import StateGraph, START, END
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class AgentState(TypedDict):
+    messages: List[Union[HumanMessage, AIMessage]]
+
+
+llm = ChatOpenAI(
+    api_key=os.getenv("OPENAI_API_KEY"),
+    base_url="https://openrouter.ai/api/v1",
+    model="openai/gpt-4o-mini",
+)
+
+def process(state: AgentState)-> AgentState:
+    """This node will solve the request you input"""
+
+    response = llm.invoke(state["messages"])
+    
+    state["messages"].append(AIMessage(content=response.content))
+    print(f"\nAI: {response.content}")
+    return state
+
+graph = StateGraph(AgentState)
+graph.add_node("process", process)
+graph.add_edge(START,"process")
+graph.add_edge("process", END)
+agent=graph.compile()
+
+conversation_history = []
+
+user_input = input("Enter: ")
+
+while user_input != "exit":
+    conversation_history.append(HumanMessage(content=user_input))
+    result = agent.invoke({"messages": conversation_history})
+    conversation_history = result["messages"]
+    user_input = input("Enter: ")
